3c9af36ed78ebc547b5e5efe469431784c85352a
Nate Thompson <nthompson2@avidxchange.com>
2022-07-16 17:25:04 -0400

    Added API Protection Solution and data validation

---
+++ ApiProtection/Models/UserModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiProtection.Models
+{
+    public class UserModel
+    {
+        [Range(1,int.MaxValue)]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string LastName { get; set; }
+        
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+        
+        [Phone]
+        public string PhoneNumber { get; set; }
+        
+        [Url]
+        public string Website { get; set; }
+        
+        [Range(0,40)]
+        public int NumberOfVehicles { get; set; }
+    }
+}
