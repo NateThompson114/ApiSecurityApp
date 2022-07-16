d78f7e05aaaba2079b44f7d5c81385d881e31796
Nate Thompson <nthompson2@avidxchange.com>
2022-07-16 16:09:34 -0400

    Added Health Check

---
+++ MonitoringApi/HealthChecks/RandomHealthCheck.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace MonitoringApi.HealthChecks
+{
+    public class RandomHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            var responseTimeInMs = Random.Shared.Next(20, 600);
+
+            return responseTimeInMs switch {
+                < 100 => Task.FromResult(HealthCheckResult.Healthy($"The response time is excellent ({responseTimeInMs}ms)")),
+                
+                < 200 => Task.FromResult(HealthCheckResult.Degraded($"The response time is greater than expected ({responseTimeInMs}ms)")),
+
+                _ => Task.FromResult(HealthCheckResult.Unhealthy($"The response time is unacceptable ({responseTimeInMs}ms)"))
+            };
+        }
+    }
+}
--- MonitoringApi/Program.cs
+++ MonitoringApi/Program.cs
@@ -1,3 +1,5 @@
+using MonitoringApi.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -8,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks()
+    .AddCheck<RandomHealthCheck>("Site Health Check")
+    .AddCheck<RandomHealthCheck>("Database Health Check");
 
 var app = builder.Build();
 
@@ -23,5 +28,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHealthChecks("/health");
 
 app.Run();
