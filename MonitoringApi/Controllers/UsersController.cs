acc2c90b787eb73c4e4890f6ba624f6162f7be7d
Nate Thompson <nthompson2@avidxchange.com>
2022-07-16 15:46:12 -0400

    Adding logging to to specific controller

--- MonitoringApi/Controllers/UsersController.cs
+++ MonitoringApi/Controllers/UsersController.cs
@@ -8,6 +8,13 @@ namespace MonitoringApi.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+
         // GET: api/<UsersController>
         [HttpGet]
         public IEnumerable<string> Get()
@@ -17,9 +24,33 @@ namespace MonitoringApi.Controllers
 
         // GET api/<UsersController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            //if (id is < 0 or > 100)
+            //{
+            //    // By putting not using string interpolation, this log will be a structured error, with a parameter of Identifier with a value, you can then search it
+            //    _logger.LogWarning("The given id of {Identifier} was invalid.", id);
+
+            //    return BadRequest("The index was out of range.");
+            //}
+                
+            //_logger.LogInformation(@"The api\users\{id} was called", id);
+            //return Ok($"Value{id}");
+            try
+            {
+                if (id is < 0 or > 100)
+                    throw new ArgumentOutOfRangeException(nameof(id));
+                
+                _logger.LogInformation(@"The api\users\{id} was called", id);
+                return Ok($"Value{id}");
+            }
+            catch (Exception ex)
+            {
+                // By putting not using string interpolation, this log will be a structured error, with a parameter of Identifier with a value, you can then search it
+                _logger.LogError(ex, "The given id of {Identifier} was invalid.", id);
+                return BadRequest("The index was out of range.");
+
+            }
         }
 
         // POST api/<UsersController>
