9c4c11365fe719161ca9b6e59aca656d31fc9848
Nate Thompson <nthompson2@avidxchange.com>
2022-12-16 16:30:07 -0500

    Added Order Routes Example

---
+++ MinimalApi/Routes/OrderRoutes.cs
@@ -0,0 +1,25 @@
+﻿using MinimalApi.Library.DataAccess;
+
+namespace MinimalApi.Routes
+{
+    public static class OrderRoutes
+    {
+        public static void AddOrderRoutes(this WebApplication app)
+        {
+            app.MapGet("/api/GetOrders", (IDataAccess data) =>
+            {
+                return Results.Ok(data.GetOrders());
+            });
+
+            app.MapGet("/api/GetOrder/{id}", (IDataAccess data, Guid id) =>
+            {
+                return Results.Ok(data.GetOrder(id));
+            });
+
+            app.MapGet("/api/GetCustomerOrders/{id}", (IDataAccess data, Guid customerId) =>
+            {
+                return Results.Ok(data.GetCustomerOrders(customerId));
+            });
+        }
+    }
+}
